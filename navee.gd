ecb6261849311bda41646838b1154e8cafe31c29
milpajaros <hector.martinar@gmail.com>
2016-10-13 22:27:18 +0100

    Add files via upload

--- navee.gd
+++ navee.gd
@@ -3,50 +3,60 @@ extends KinematicBody2D
 # member variables here, example:
 # var a=2
 # var b="textvar"
-var velx = 0
-var vely = 0
-var velmin = 100
-var velxmax= 500
-var velymax= 500
-var acc = 50
+var acceleration = 4000
+var maxspeed = 400
+var velocity = Vector2()
+var slowspeed= 0
 func _ready():
     # Called every time the node is added to the scene.
     # Initialization here
-    set_process(true)
+    set_fixed_process(true)
 
-func _process(delta):
+func _fixed_process(delta):
     var mousepos = get_global_mouse_pos()
     look_at(mousepos)
-    if(!Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right")):
-        velx= velmin
+
+    if(Input.is_action_pressed("ui_left")):
+        if(velocity.x > -maxspeed):
+            velocity.x -= delta*acceleration
+    if(Input.is_action_pressed("ui_right")):
+        if(velocity.x < maxspeed):
+            velocity.x += delta*acceleration
+    if(Input.is_action_pressed("ui_up")):
+        if(velocity.y > -maxspeed):
+            velocity.y -= delta*acceleration
+    if(Input.is_action_pressed("ui_down")):
+        if(velocity.y < maxspeed):
+            velocity.y += delta*acceleration
+#Deseleracion
+    if(!Input.is_action_pressed("ui_left") && (velocity.x !=slowspeed) && !Input.is_action_pressed("ui_right")):
+       
+         if (velocity.x>0):
+            if (velocity.x - (delta*acceleration) <0):
+                velocity.x=0
+            else:       
+                velocity.x -= (delta*acceleration)
+         else:
+            if (velocity.x + (delta*acceleration) <0):
+                velocity.x=0
+            else:       
+                velocity.x += (delta*acceleration)
+
+    if(!Input.is_action_pressed("ui_up") && (velocity.y !=slowspeed)&& !Input.is_action_pressed("ui_down")):
+       
+        if (velocity.y>0):
+            if (velocity.y - (delta*acceleration) <0):
+                velocity.y=0
+            else:       
+                velocity.y -= (delta*acceleration)
+        else:
+            if (velocity.y + (delta*acceleration) <0):
+                velocity.y=0
+            else:       
+                velocity.y += (delta*acceleration)
+            
+    
 
 
-    elif(Input.is_action_pressed("ui_left")):
-        if (velx>-velxmax):
-            if velx>0:
-                velx=velx/2
-            velx=velx-acc
-        move(Vector2((velx)*delta,0))
-    elif(Input.is_action_pressed("ui_right")):
-        if (velx<velxmax):
-            if velx<0:
-	            velx=velx/2
-            velx= velx + acc
-        move(Vector2((velx)*delta,0))
-
-    if(!Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down")):
-        vely= velmin
-
-    elif(Input.is_action_pressed("ui_up")):
-        if (vely>-velymax):
-            if vely>0:
-                vely=vely/2
-            vely=vely-acc
-        move(Vector2(0,(vely)*delta))
-    elif(Input.is_action_pressed("ui_down")):
-        if (vely<velymax):
-            if vely<0:
-                vely=vely/2
-            vely = vely + acc
-        move(Vector2(0,(vely)*delta))
-        
+    var movement = velocity*delta
+    move(movement)
