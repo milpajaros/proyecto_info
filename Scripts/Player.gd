1c569e31c173998bcd3080a1ce2b665b8ce73ddb
Angehl <angehl@gmail.com>
2016-10-18 00:26:46 +0100

    Anotaciones

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
+#variables para el disparo
 onready var laser_scene = preload("res://scenes/laser_scene.xml")
 var laser
 
-var acceleration = 4000
-var maxspeed = 400
-var velocity = Vector2()
-var slowspeed= 0
-var shootcd = 0.5
+var acceleration = 4000 #aceleración del player
+var maxspeed = 400 #velocidad máxima
+var velocity = Vector2() #vector velocidad
+var shootcd = 0.15 #tiempo entre disparo y disparo
 var actualcd = 0
 
 
@@ -17,8 +17,9 @@ func _ready():
 func _fixed_process(delta):
 	actualcd -= delta
 	var mousepos = get_global_mouse_pos()
-	look_at(mousepos)
+	look_at(mousepos) #la nave apunta al raton
 	
+	#deteccion de movimiento
 	if(Input.is_action_pressed("ui_left")):
 		if(velocity.x > -maxspeed):
 			velocity.x -= delta*acceleration
@@ -31,21 +32,23 @@ func _fixed_process(delta):
 	if(Input.is_action_pressed("ui_down")):
 		if(velocity.y < maxspeed):
 			velocity.y += delta*acceleration
-
+	
+	#condicion de parada
 	if(!Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right")):
 		velocity.x=0
 	if(!Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down")):
 		velocity.y=0        
 	
-	var movement = velocity*delta
-	movement =move(movement)
+	#Deteccion de colisiones
 	if (is_colliding()):
 		var n = get_collision_normal()
-		movement = n.slide(movement)
 		velocity = n.slide(velocity)
 
+	move(velocity*delta)
+	
+	#mecanica del disparo
 	if(Input.is_action_pressed("ui_accept") && (actualcd <= 0)):
-		actualcd = shootcd
+		actualcd = shootcd #reinicia el CD
 		laser = laser_scene.instance()
 		var playerpos = get_node("PlayerSprite").get_pos()
 		var LaserSpawnPoint = get_node("LaserSpawnPoint").get_global_pos()
