487aec78948e4fc88a9264bb12dfc87b66a087fa
Héctor Martín <hector.martinar@gmail.com>
2016-10-23 21:58:06 +0100

    para k kieres saber eso jaja saludos

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -4,12 +4,14 @@ extends Node
 var timer
 onready var enemy_scene = preload("res://scenes/enemy.xml")
 var enemy
+var enemynode
 
 func _ready():
 	timer = get_node("Enemy_1_spawnrate")
 	timer.set_wait_time(4)
 	timer.connect("timeout",self, "_timeout")
 	timer.start()
+	enemynode = get_tree().get_root().get_node("Root/Enemy/Enemy_spawn_point")
 	set_fixed_process(true)
 func _fixed_process(delta):
 	
@@ -21,13 +23,13 @@ func _fixed_process(delta):
 	
 func _timeout():
 	enemy= enemy_scene.instance()
-	var enemypos=get_node("Enemy/Enemy_spawn_point").get_global_pos()
+	var enemypos=enemy.get_global_pos()
+
 	var enemyholder = get_node("Enemy_1_holder")
 	enemyholder.add_child(enemy)
 	enemy.set_pos(enemypos)
 	#es infinito hay que ponerle condicion de parada 
 	timer.set_wait_time(4)
-	timer.connect("timeout",self, "_timeout")
 	timer.start()
 
 	
