6074f212b200017d3da94af8a8dc11a0c184a2eb
Angehl <angehl@gmail.com>
2016-10-24 00:01:48 +0100

    HP otra vez

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -7,33 +7,20 @@ var enemy
 var enemynode
 
 func _ready():
-	timer = get_node("Enemy_1_spawnrate")
+	timer = get_node("EnemySpawnPoint/EnemySpawnrate")
 	timer.set_wait_time(4)
 	timer.connect("timeout",self, "_timeout")
 	timer.start()
-	enemynode = get_tree().get_root().get_node("Root/Enemy/Enemy_spawn_point")
+	enemynode = get_node("EnemySpawnPoint")
 	set_fixed_process(true)
-func _fixed_process(delta):
-	
-	pass
-	
-	
-	
-	
-	
+
 func _timeout():
 	enemy= enemy_scene.instance()
 	var enemypos=enemy.get_global_pos()
 
-	var enemyholder = get_node("Enemy_1_holder")
+	var enemyholder = get_node("EnemySpawnPoint/EnemyHolder")
 	enemyholder.add_child(enemy)
 	enemy.set_pos(enemypos)
 	#es infinito hay que ponerle condicion de parada 
 	timer.set_wait_time(4)
 	timer.start()
-
-	
-
-
-
-
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+const MAX_HP = 10
 #variables para el disparo
 onready var laser_scene = preload("res://scenes/laser_scene.xml")
 var laser
@@ -9,16 +10,27 @@ var maxspeed = 400 #velocidad máxima
 var velocity = Vector2() #vector velocidad
 var shootcd = 0.15 #tiempo entre disparo y disparo
 var actualcd = 0
+var dead = false
+var hp = 10
 
 
 func _ready():
+	get_node("ExplosionAnimation").set_hidden(true)
 	set_fixed_process(true)
 
 
 func _fixed_process(delta):
+	get_parent().get_node("GUI/HPBar").set_val(hp*100/MAX_HP)
 	actualcd -= delta
 	var mousepos = get_global_mouse_pos()
-	look_at(mousepos) #la nave apunta al raton
+	if(!dead):
+		look_at(mousepos) #la nave apunta al raton
+		
+	if(hp <= 0):
+		actualcd = delta
+		if(!dead):
+			dead = true
+			_die()
 	
 	#deteccion de movimiento
 	if(Input.is_action_pressed("ui_left")):
@@ -58,3 +70,16 @@ func _fixed_process(delta):
 		laser.set_meta("aliado",1)
 		laser.set_pos(LaserSpawnPoint)
 		laser.look_at(get_global_mouse_pos())
+
+func _on_BulletHitArea_body_enter( body ):
+	if(body.has_method("_hit") && body.has_meta("enemigo")):
+		hp -=1;
+		body._hit()
+
+func _die():
+	acceleration = 0
+	velocity = Vector2(0,0)
+	get_node("PlayerAnimation").set_hidden(true)
+	get_node("ExplosionAnimation").set_hidden(false)
+	get_node("ExplosionAnimation").set_frame(0)
+	get_node("ExplosionAnimation").play("default")
